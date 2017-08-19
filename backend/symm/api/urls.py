b82af61a69947a4c9e3d59ca59dd8b76c91a73c7
Kristaps Rāts <kristaps.rats@gmail.com>
2017-08-19 22:43:54 +0300

    Appointments

--- backend/symm/api/urls.py
+++ backend/symm/api/urls.py
@@ -1,10 +1,11 @@
 from rest_framework import routers
 
-from api.views import ServiceCategoryViewSet, ProviderViewSet, ServiceViewSet
+from api.views import ServiceCategoryViewSet, ProviderViewSet, ServiceViewSet, AppointmentViewSet
 
 router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'providers', ProviderViewSet, base_name='providers')
 router.register(r'categories', ServiceCategoryViewSet, base_name='categories')
 router.register(r'services', ServiceViewSet, base_name='services')
+router.register(r'appointments', AppointmentViewSet, base_name='appointments')
 
 urlpatterns = router.urls
